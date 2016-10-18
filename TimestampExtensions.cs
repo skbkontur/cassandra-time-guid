f8b98972e34e2da69a762d208d3be40ce6948257
burmistrov <burmistrov@skbkontur.ru>
2016-10-18 17:39:46 +0500

    Moved TimestampExtensions close to Timestamp

---
+++ TimestampExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using JetBrains.Annotations;
+
+namespace SKBKontur.Catalogue.Objects
+{
+    public static class TimestampExtensions
+    {
+        [NotNull]
+        public static Timestamp Floor([NotNull] this Timestamp timestamp, TimeSpan precision)
+        {
+            if (precision.Ticks <= 0)
+                throw new InvalidProgramStateException("Could not run Floor with zero precision");
+            return new Timestamp((timestamp.Ticks / precision.Ticks) * precision.Ticks);
+        }
+    }
+}
