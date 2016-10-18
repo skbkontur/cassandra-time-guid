96c4c48836e887133025d7e420d7fafdd735a571
burmistrov <burmistrov@skbkontur.ru>
2016-10-18 17:45:58 +0500

    The value of precision added into exception message

--- TimestampExtensions.cs
+++ TimestampExtensions.cs
@@ -10,7 +10,7 @@ namespace SKBKontur.Catalogue.Objects
         public static Timestamp Floor([NotNull] this Timestamp timestamp, TimeSpan precision)
         {
             if (precision.Ticks <= 0)
-                throw new InvalidProgramStateException("Could not run Floor with zero precision");
+                throw new InvalidProgramStateException(string.Format("Could not run Floor with {0} precision", precision));
             return new Timestamp((timestamp.Ticks / precision.Ticks) * precision.Ticks);
         }
     }
